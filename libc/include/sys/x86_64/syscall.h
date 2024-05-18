220fdf7b49a93c0793cee435d362634eca3629ad
Evan-Kreutzwiser <53940800+Evan-Kreutzwiser@users.norepl.github.com>
2024-05-17 22:27:55 -0400

    Preserve user registers in syscalls

--- libc/include/sys/x86_64/syscall.h
+++ libc/include/sys/x86_64/syscall.h
@@ -14,20 +14,20 @@ extern "C" {
 
 static inline int _syscall_1(long syscall_num, long arg0) {
     int result;
-    asm volatile ("syscall" : "=a" (result) : "D" (syscall_num), "S" (arg0) : "r8", "r9", "r10", "r11", "rcx", "rcx", "memory");
+    asm volatile ("syscall" : "=a" (result) : "D" (syscall_num), "S" (arg0) : "rcx", "r11", "memory");
     return result;
 }
 
 static inline int _syscall_2(long syscall_num, long arg0, long arg1) {
     int result;
-    asm volatile ("syscall" : "=a" (result) : "D" (syscall_num), "S" (arg0), "d" (arg1) : "r8", "r9", "r10", "r11", "rcx", "memory");
+    asm volatile ("syscall" : "=a" (result) : "D" (syscall_num), "S" (arg0), "d" (arg1) : "rcx", "r11", "memory");
     return result;
 }
 
 static inline int _syscall_3(long syscall_num, long arg0, long arg1, long arg2) {
     int result;
     register long r10 asm("r10") = arg2;
-    asm volatile ("syscall" : "=a" (result) : "D" (syscall_num), "S" (arg0), "d" (arg1), "r" (r10) : "r8", "r9", "r11", "rcx", "memory");
+    asm volatile ("syscall" : "=a" (result) : "D" (syscall_num), "S" (arg0), "d" (arg1), "r" (r10) : "rcx", "r11", "memory");
     return result;
 }
 
@@ -35,7 +35,7 @@ static inline int _syscall_4(long syscall_num, long arg0, long arg1, long arg2, 
     int result;
     register long r10 asm("r10") = arg2;
     register long r8 asm("r8") = arg3;
-    asm volatile ("syscall" : "=a" (result) : "D" (syscall_num), "S" (arg0), "d" (arg1), "r" (r10), "r" (r8) :  "r9", "r11", "rcx", "memory");
+    asm volatile ("syscall" : "=a" (result) : "D" (syscall_num), "S" (arg0), "d" (arg1), "r" (r10), "r" (r8) :  "rcx", "r11", "memory");
     return result;
 }
 
@@ -44,7 +44,7 @@ static inline int _syscall_5(long syscall_num, long arg0, long arg1, long arg2, 
     register long r10 asm("r10") = arg2;
     register long r8 asm("r8") = arg3;
     register long r9 asm("r9") = arg4;
-    asm volatile ("syscall" : "=a" (result) : "D" (syscall_num), "S" (arg0), "d" (arg1), "r" (r10), "r" (r8), "r" (r9) : "r11", "rcx", "memory");
+    asm volatile ("syscall" : "=a" (result) : "D" (syscall_num), "S" (arg0), "d" (arg1), "r" (r10), "r" (r8), "r" (r9) : "rcx", "r11", "memory");
     return result;
 }
 #ifdef __cplusplus
